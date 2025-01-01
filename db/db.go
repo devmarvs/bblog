255018aedf6929d797a368a5d7351c2a7ab7fd19
devmarvs <marvin.dev.ph@gmail.com>
2025-01-01 22:59:35 +0800

    updated create users table

--- db/db.go
+++ db/db.go
@@ -23,11 +23,12 @@ func InitDb() {
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 	dbUser := os.Getenv("DB_USER")
-	dbPass := os.Getenv("DB_PASS")
+	dbPass := os.Getenv("DB_PWORD")
 	dbName := os.Getenv("DB_NAME")
 	connString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPass, dbName)
+
 	// Open connection to PostgreSQL
-	DB, err := sql.Open("postgres", connString)
+	DB, err = sql.Open("postgres", connString)
 	if err != nil {
 		// log.Fatal("Error connecting to database:", err)
 		panic("Could not connect to database.")
@@ -44,7 +45,7 @@ func createTables() {
 
 	createUsersTable := `
 		CREATE TABLE IF NOT EXISTS bblog.users (
-			user_id BIGSERIAL,
+			user_id BIGSERIAL PRIMARY KEY,
 			created_ts TIMESTAMPTZ NULL DEFAULT NOW(),
 			updated_ts TIMESTAMPTZ NULL,
 			user_type_id INTEGER NULL,
@@ -56,12 +57,18 @@ func createTables() {
 			is_active BOOL NOT NULL DEFAULT FALSE,
 			is_deleted BOOL NOT NULL DEFAULT FALSE,
 			is_premium BOOL NOT NULL DEFAULT FALSE
-		)
+		);
+		CREATE INDEX bb_user_idx ON bblog.users USING btree(user_id,created_ts, updated_ts,email,mobile);
 	`
 
 	_, err := DB.Exec(createUsersTable)
 
 	if err != nil {
-		panic("Could not create events table")
+		log.Fatalf("Error creating users table: %v", err) // Log the real error
+
 	}
+
+	// if err != nil {
+	// 	panic("Could not create users table")
+	// }
 }
